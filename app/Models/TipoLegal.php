aa4941ccc298727707e4aa737ad877b35c7dcb08
heredia95 <limber2000h@gmail.com>
2024-08-02 11:19:22 -0400

    ajustes tipo legal

--- app/Models/TipoLegal.php
+++ app/Models/TipoLegal.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\CommonScopes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TipoLegal extends Model
 {
-    use HasFactory;
+    use CommonScopes, HasFactory;
     protected $fillable=[
         'nombre',
         'abreviatura',
