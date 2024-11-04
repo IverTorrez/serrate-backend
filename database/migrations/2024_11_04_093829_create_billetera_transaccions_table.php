a834b16a75dbb9cde194ea22e64bea58f707a356
IverTorrez <ivermenacho123@gmail.com>
2024-11-04 17:39:19 -0400

    servicio de transacciones de billetera

---
+++ database/migrations/2024_11_04_093829_create_billetera_transaccions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('billetera_transaccions', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('monto',10,2)->comment('monto de la transaccion');
+            $table->timestamp('fecha_transaccion')->nullable()->comment('fecha de la transaccion');
+            $table->string('tipo', 20)->comment('tipo de transaccin, DEPOSITO, RETIRO');
+            $table->integer('billetera_id')->comment('id de la tabla billeteras');
+            $table->integer('usuario_id')->comment('id de la tabla users, usuario quien hizo la transaccion');
+            $table->string('estado', 20)->comment('estado ACTIVO,INACTIVO');
+            $table->integer('es_eliminado')->comment('1 es eliminado, 0 no es eliminado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('billetera_transaccions');
+    }
+};
