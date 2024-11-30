ddc693b7196c6a46b92ad88dea7f9233e41b9762
heredia95 <limber2000h@gmail.com>
2024-11-30 18:31:11 -0400

    cerrar sesion

--- app/Constants/GeneralMessages.php
+++ app/Constants/GeneralMessages.php
@@ -7,6 +7,7 @@ class GeneralMessages
     public const ERROR_GENERICO = 'Ha ocurrido un error inesperado.';
     public const EXITO_GENERICO = 'Operación exitosa.';
     public const INICIO_SESION_EXITOSO = 'Inicio de sesión exitoso.';
+    public const CIERRE_SESION_EXITOSO = 'Cierrre de sesión exitoso.';
     public const REGISTRO_EXITOSO = 'Registro completado exitosamente.';
     public const ACTUALIZACION_EXITOSA = 'Actualización realizada con éxito.';
     public const ELIMINACION_EXITOSA = 'Eliminación completada con éxito.';
